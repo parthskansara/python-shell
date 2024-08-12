9c8859febe7ba9c859b6bed52843ecc0ab0db37e
Parth Kansara <parthskansara@gmail.com>
2024-08-11 18:42:05 -0700

    Added type command

--- app/main.py
+++ app/main.py
@@ -1,9 +1,30 @@
 import sys
 
+def invalid_command(command):
+    print(f"{command}: command not found")
+
+def exit(args, exit_status):    
+    if args:
+        exit_status = args[0]
+    print(f"exit status: {exit_status}")
+
+def echo(args):
+    for arg in args:
+        print(f"{arg}", end= " ")
+    print("\b")
+
+def type_command(command, args, command_set):
+    args = " ".join(args)
+    if args in command_set:
+        print(f"{args} is a shell builtin")
+
+    else:
+        print(f"{args}: not found")
+
+
 
 def main():
-
-    command_set = set()
+    command_set = set(["exit", "echo", "type"])
     exit_status = ""
     # Uncomment this block to pass the first stage
     run = True
@@ -15,12 +36,20 @@ def main():
         user_input = input()
         command, args = user_input.split(" ")[0], user_input.split(" ")[1:]
 
+        if command not in command_set:
+            invalid_command(command)
+            continue
+
         if command == "exit":
             run = False
-            exit_status = args[0]
-            print(f"exit_status: {exit_status}")
-        else:
-            print(f"{command}: command not found")
+            exit(args, exit_status)
+
+        elif command == "echo":
+            echo(args)
+
+        elif command == "type":
+            type_command(command, args, command_set)
+
 
 
 if __name__ == "__main__":
