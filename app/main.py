8e27a61928e212c0462daf3e437b449d8c49e65d
Parth Kansara <parthskansara@gmail.com>
2024-08-11 17:20:11 -0700

    Handle missing commands

--- app/main.py
+++ app/main.py
@@ -2,12 +2,17 @@ import sys
 
 
 def main():
+
+    command_set = set()
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    command = input()
+
+    if command not in command_set:
+        sys.stdout.write(command + ": command not found")
 
 
 if __name__ == "__main__":
