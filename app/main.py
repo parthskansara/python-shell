9f829aa6df8f44065ab2b47920b3db6882cf46f9
Parth Kansara <parthskansara@gmail.com>
2024-08-11 17:34:59 -0700

    exit builtin

--- app/main.py
+++ app/main.py
@@ -4,16 +4,23 @@ import sys
 def main():
 
     command_set = set()
+    exit_status = ""
     # Uncomment this block to pass the first stage
-
-    while(1):
+    run = True
+    while(run):
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
         # Wait for user input
-        command = input()
+        user_input = input()
+        command, args = user_input.split(" ")[0], user_input.split(" ")[1:]
 
-        print(f"{command}: command not found")
+        if command == "exit":
+            run = False
+            exit_status = args[0]
+            print(f"exit_status: {exit_status}")
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
